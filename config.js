998bd2a0594d371a4a90f1a41c1e5834562f0a81
Will <master_wu51@hotmail.com>
2019-11-26 20:56:08 +1000

    try to fix bugs

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  MONGODB:
-    "mongodb+srv://merng-admin:merng-admin@cluster0-sfhgp.mongodb.net/merng?retryWrites=true",
-  SECRET_KEY: "developer mode"
+  MONGODB: "",
+  SECRET_KEY: ""
 };
